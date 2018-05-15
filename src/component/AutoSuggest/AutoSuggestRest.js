e3f6ddcb962ace9069ccab74fed111c8b91661b7
Dhilip kumar Velusamy <dhilip1211@gmail.com>
2018-05-15 19:06:31 +0530

    Update AutoSuggestRest.js

--- src/component/AutoSuggest/AutoSuggestRest.js
+++ src/component/AutoSuggest/AutoSuggestRest.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 const AutoSuggestRest = (searchTerm) => {
     return $.ajax({
-        url: "https://voyager.goibibo.com/api/v1/flights_search/find_node_by_name_v2/?search_query=" + searchTerm + "&limit=10",
+        url: "",
         type : "GET"
     }).then(response => response);
 }
